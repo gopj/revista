279420f515d2915f3a0df0b768e8b41a9bd20a16
Jesús González <gopixc@gmail.com>
2016-05-22 18:16:11 -0500

    sql

--- revista.sql
+++ revista.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `especialidades`
+--
+
+DROP TABLE IF EXISTS `especialidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `especialidades` (
+  `id_especialidad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_especialidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especialidades`
+--
+
+LOCK TABLES `especialidades` WRITE;
+/*!40000 ALTER TABLE `especialidades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `especialidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `lugares`
 --
 
@@ -25,12 +48,12 @@ DROP TABLE IF EXISTS `lugares`;
 CREATE TABLE `lugares` (
   `id_lugar` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(40) NOT NULL,
-  `telefono` int(10) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
   `direccion` text NOT NULL,
   `lat` float(10,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
   PRIMARY KEY (`id_lugar`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +62,7 @@ CREATE TABLE `lugares` (
 
 LOCK TABLES `lugares` WRITE;
 /*!40000 ALTER TABLE `lugares` DISABLE KEYS */;
-INSERT INTO `lugares` VALUES (1,'Lugar 1',1,'Av ElÃ­as Zamora 18, Valle de Las Garzas, I, 28219 Manzanillo, Col., Mexico',19.088053,-104.297455),(2,'Lugar 2',2,'Congreso de Chilpancingo 16, Col del PacÃ­fico, 28879 Manzanillo, Col., Mexico',19.083469,-104.306381);
+INSERT INTO `lugares` VALUES (1,'Lugar 1','1315454878','Av ElÃ­as Zamora 16D, Valle de Las Garzas, I, 28219 Manzanillo, Col., Mexico',19.088051,-104.297615),(2,'Lugar 2','2132165468','Congreso de Chilpancingo 16, Col del PacÃ­fico, 28879 Manzanillo, Col., Mexico',19.083469,-104.306381),(3,'Lugar 3','3213216546','Av Paseo de Las Gaviotas 251, Soleares, Viveros Pelayo, 28219 Manzanillo, Col., Mexico',19.101961,-104.326332),(4,'Lugar 4','3213216516','Calle 21 de Marzo 275, Centro HistÃ³rico, 28200 Manzanillo, Col., Mexico',19.050964,-104.316917);
 /*!40000 ALTER TABLE `lugares` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,9 +79,8 @@ CREATE TABLE `markers` (
   `address` varchar(80) NOT NULL,
   `lat` float(10,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
-  `type` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +89,7 @@ CREATE TABLE `markers` (
 
 LOCK TABLES `markers` WRITE;
 /*!40000 ALTER TABLE `markers` DISABLE KEYS */;
-INSERT INTO `markers` VALUES (1,'Pan Africa Market','1521 1st Ave, Seattle, WA',47.608940,-122.340141,'restaurant'),(2,'Buddha Thai & Bar','2222 2nd Ave, Seattle, WA',47.613590,-122.344391,'bar'),(3,'The Melting Pot','14 Mercer St, Seattle, WA',47.624561,-122.356445,'restaurant'),(4,'Ipanema Grill','1225 1st Ave, Seattle, WA',47.606365,-122.337654,'restaurant'),(5,'Sake House','2230 1st Ave, Seattle, WA',47.612823,-122.345673,'bar'),(6,'Crab Pot','1301 Alaskan Way, Seattle, WA',47.605961,-122.340363,'restaurant'),(7,'Mama\'s Mexican Kitchen','2234 2nd Ave, Seattle, WA',47.613976,-122.345467,'bar'),(8,'Wingdome','1416 E Olive Way, Seattle, WA',47.617214,-122.326584,'bar'),(9,'Piroshky Piroshky','1908 Pike pl, Seattle, WA',47.610126,-122.342834,'restaurant');
+INSERT INTO `markers` VALUES (1,'Frankie Johnnie & Luigo Too','939 W El Camino Real, Mountain View, CA',37.386337,-122.085823),(2,'Amici\'s East Coast Pizzeria','790 Castro St, Mountain View, CA',37.387138,-122.083237),(3,'Kapp\'s Pizza Bar & Grill','191 Castro St, Mountain View, CA',37.393887,-122.078918),(4,'Round Table Pizza: Mountain View','570 N Shoreline Blvd, Mountain View, CA',37.402653,-122.079353),(5,'Tony & Alba\'s Pizza & Pasta','619 Escuela Ave, Mountain View, CA',37.394012,-122.095528),(6,'Oregano\'s Wood-Fired Pizza','4546 El Camino Real, Los Altos, CA',37.401726,-122.114647);
 /*!40000 ALTER TABLE `markers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,7 +105,7 @@ CREATE TABLE `medico_lugares` (
   `id_medico` int(11) NOT NULL,
   `id_lugar` int(11) NOT NULL,
   PRIMARY KEY (`id_medico_lugar`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,6 +114,7 @@ CREATE TABLE `medico_lugares` (
 
 LOCK TABLES `medico_lugares` WRITE;
 /*!40000 ALTER TABLE `medico_lugares` DISABLE KEYS */;
+INSERT INTO `medico_lugares` VALUES (12,2,1),(13,2,4);
 /*!40000 ALTER TABLE `medico_lugares` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,7 +132,7 @@ CREATE TABLE `medicos` (
   `correo` varchar(45) DEFAULT NULL,
   `imagen` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`id_medico`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +141,7 @@ CREATE TABLE `medicos` (
 
 LOCK TABLES `medicos` WRITE;
 /*!40000 ALTER TABLE `medicos` DISABLE KEYS */;
-INSERT INTO `medicos` VALUES (2,'Figo','Gonzalez','figo@rev.com','/images/medicos/default.png');
+INSERT INTO `medicos` VALUES (2,'Figo','Gonzalez','figo@rev.com','/images/medicos/1463813842.jpg'),(3,'Chuy','G','G','/images/medicos/1463900258.jpg'),(5,'t','t','t','/images/medicos/1463904286.jpg');
 /*!40000 ALTER TABLE `medicos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -131,4 +154,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-20  1:56:00
+-- Dump completed on 2016-05-22 18:15:53
